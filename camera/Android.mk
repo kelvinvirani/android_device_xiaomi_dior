7abce1fef2e2048c435fee3b370713291103d0f9
subdragonzj <zj502407679@gmail.com>
2019-05-30 23:05:31 +0800

    dior: camera: import oss camera hal from LNX.LA.3.5.2.2.2-00510-8x26.0

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,37 +1,14 @@
-LOCAL_PATH := $(call my-dir)
+MM_V4L2_DRIVER_LIST += msm8960
+MM_V4L2_DRIVER_LIST += msm8974
+MM_V4L2_DRIVER_LIST += msm8226
+MM_V4L2_DRIVER_LIST += msm8610
+MM_V4L2_DRIVER_LIST += msm_bronze
+MM_V4L2_DRIVER_LIST += msm8916
 
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
-    CameraWrapper.cpp
-
-LOCAL_C_INCLUDES := \
-    framework/native/include \
-    system/media/camera/include
-
-LOCAL_SHARED_LIBRARIES := \
-    libhardware \
-    libbase \
-    libhidltransport \
-    liblog \
-    libcamera_client \
-    libutils \
-    libcutils \
-    libgui \
-    libdl \
-    android.hidl.token@1.0-utils \
-    android.hardware.graphics.bufferqueue@1.0
-
-LOCAL_HEADER_LIBRARIES := libnativebase_headers
-
-LOCAL_STATIC_LIBRARIES := \
-    libarect \
-	libbase
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_MODULE := camera.msm8226
-
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_SHARED_LIBRARY)
+ifeq ($(call is-board-platform-in-list,$(MM_V4L2_DRIVER_LIST)),true)
+  ifneq ($(USE_CAMERA_STUB),true)
+    ifneq ($(BUILD_TINY_ANDROID),true)
+      include $(call all-subdir-makefiles)
+    endif
+  endif
+endif
